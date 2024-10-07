2d4250c2001b94d940952bb71b8b07da49264c23
jesus alberto castañeda <castaedabs@gmail.com>
2024-10-07 12:41:53 -0500

    Añadir campo de WhatsApp al formulario de registro y enlace a Coinsumo

--- models/User.js
+++ models/User.js
@@ -5,21 +5,19 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  whatsapp: { type: String }, // Añadir campo WhatsApp
   sponsorId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   front: { type: Number, default: 0 },
   level: { type: Number, default: 1 },
   position: { type: Number, default: 0 },
   points: { type: Number, default: 0 },
   wallet: { type: mongoose.Schema.Types.ObjectId, ref: 'Wallet' },
-  // Puedes agregar un campo para métodos de pago específicos si es necesario
   paymentMethods: {
     cash: { type: Boolean, default: true }, // Permitir pago en efectivo
     qrCode: { type: Boolean, default: true } // Permitir pago por QR
   },
-  // ... otros campos del usuario
-  membershipStatus: { type: String, default: 'inactive' }, // Estado de membresía (active, inactive, suspended)
-  membershipExpiryDate: { type: Date } ,// Fecha de expiración de membresía
-  // ... otros campos del usuario
+  membershipStatus: { type: String, default: 'inactive' }, // Estado de membresía
+  membershipExpiryDate: { type: Date },
   entrepreneur: { type: Boolean, default: false },  // Indica si es un empresario
   wholesale: { type: Boolean, default: false }, // Indica si es un mayorista
   collaborator: { type: Boolean, default: false } // Indica si es un colaborador
@@ -28,3 +26,4 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+ 
--- src/scripts.js
+++ src/scripts.js
@@ -9,15 +9,15 @@ document.addEventListener('DOMContentLoaded', () => {
         const userId = document.getElementById('userId').value;
         const name = document.getElementById('name').value;
         const email = document.getElementById('email').value;
+        const whatsapp = document.getElementById('whatsapp').value; // Nueva línea
         const tipoUsuario = document.getElementById('tipo_usuario').value;
         const porcentaje = document.getElementById('porcentaje').value;
 
         // Aquí puedes manejar el envío de datos al servidor o mostrar un mensaje
-        console.log('ID de Usuario: {userId}, Nombre: {name}, Email: {email}, Tipo de Usuario: {tipoUsuario}, Porcentaje: {porcentaje}');
+        console.log('ID de Usuario: {userId}, Nombre: {name}, Email: {email}, WhatsApp: {whatsapp}, Tipo de Usuario: {tipoUsuario}, Porcentaje: {porcentaje}');
 
         // Reinicia el formulario
         registrationForm.reset();
         alert('Registro completado correctamente');
     });
 });
- 
