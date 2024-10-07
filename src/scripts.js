c0c47bb63450cd626e7e973519354c462901abb8
jesus alberto castañeda <castaedabs@gmail.com>
2024-10-07 12:19:06 -0500

    Actualizacion de los archivos HTML, CSS Y JS para registro de usuarios y afiliados

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,23 @@
-console.log("Hola desde el archivo JavaScript");
 // script.js
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('Template de Coinsumo cargado');
+    const registrationForm = document.getElementById('registrationForm');
+    
+    registrationForm.addEventListener('submit', function(e) {
+        e.preventDefault(); // Previene el comportamiento por defecto del formulario
+
+        const userId = document.getElementById('userId').value;
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const tipoUsuario = document.getElementById('tipo_usuario').value;
+        const porcentaje = document.getElementById('porcentaje').value;
+
+        // Aquí puedes manejar el envío de datos al servidor o mostrar un mensaje
+        console.log('ID de Usuario: {userId}, Nombre: {name}, Email: {email}, Tipo de Usuario: {tipoUsuario}, Porcentaje: {porcentaje}');
+
+        // Reinicia el formulario
+        registrationForm.reset();
+        alert('Registro completado correctamente');
+    });
 });
  
